1c76771a9d929f4ff3c1fd9bc4109a53dfdb3331
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-15 18:02:40 +0000

    Added switch case for bnz

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -110,6 +110,10 @@ public class Translator {
         case "out":
             r = scanInt();
             return new OutInstruction(label, r);
+        case "bnz":
+            s1 = scanInt();
+            String jumpLabel = scan();
+            return new BnzInstruction(label, jumpLabel, s1);
 		}
 
 		// You will have to write code here for the other instructions.
