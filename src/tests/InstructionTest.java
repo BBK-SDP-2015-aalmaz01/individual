b3ec5f9de6bd7a1b378d19b718ddbe98fe63a6f2
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-15 14:12:25 +0000

    Created new test class for testing the instructions

---
+++ src/tests/InstructionTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import sml.Machine;
+import sml.Translator;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class InstructionTest {
+    private Machine m = new Machine();
+    private String fileName = ("tests" + File.separator + "InstrText.txt");
+
+    @Before
+    public void BuildUp(){
+        Translator t = new Translator(fileName);
+        t.readAndTranslate(m.getLabels(), m.getProg());
+
+        System.out.println("Here is the program; it has " + m.getProg().size() + " instructions.");
+        System.out.println(m);
+
+        System.out.println("Beginning program execution.");
+        m.execute();
+        System.out.println("Ending program execution.");
+
+        System.out.println("Values of registers at program termination:");
+        System.out.println(m.getRegisters() + ".");
+    }
+
+    @Test
+    public void TestLin() {
+
+    }
+}
